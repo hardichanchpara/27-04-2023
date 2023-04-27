b68997789fe7eb7b9463a5ed6267e8717a980111
hardichanchpara <131763035+hardichanchpara@users.noreply.github.com>
2023-04-27 16:10:10 +0530

    Add files via upload

---
+++ (X-Y).C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   int x=3,y=4,xy;
+   xy=(x*x)-(2*x*y)+(y*y);
+   printf("x-y=%d",xy);
+   getch();
+}
+
---
+++ HARDI.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   clrscr();
+   int x=5,y=6,xy;
+   xy=(x*x*x)+((3*x*y)*(x+y))+(y*y*y);
+   printf("(x+y)3=%d",xy);
+   getch();
+}
+
+
